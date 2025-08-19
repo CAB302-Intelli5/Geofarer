f32b757f974224e17380032bf88b8b1dc0f76360
malign-seas <begonemoth@gmail.com>
2025-08-19 14:17:05 +1000

    commit

--- src/main/java/com/example/geofarer/HelloApplication.java
+++ src/main/java/com/example/geofarer/HelloApplication.java
@@ -8,15 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static final String TITLE = "asdf";
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("TITLE");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
